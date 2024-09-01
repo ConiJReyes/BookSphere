37015565da8a2bdd4c188928c29537b632e2c7cb
ConiJReyes <mac.reyes@duocuc.cl>
2024-09-01 19:47:38 -0400

    Proyecto finalizado para la prueba parcial 1,formulario de la contraseña listo

--- src/app/pages/registrarse/registrarse.page.ts
+++ src/app/pages/registrarse/registrarse.page.ts
@@ -8,25 +8,23 @@ import { AlertController, MenuController } from '@ionic/angular';
   styleUrls: ['./registrarse.page.scss'],
 })
 export class RegistrarsePage implements OnInit {
-
+//variables
   username: string | undefined;
   password: string | undefined;
   email: string | undefined;
   passwordR: string | undefined;
 
-  ValidacionC : boolean = false
-  ValidarLargoP : boolean = false
-  ValidarIgualdad : boolean = false
-  
-
-
-    // el alertcontroller es para las pantallaz de errores emergentes, bueno no necesariamente de errores
-  constructor(private router:Router, private alertcontroller: AlertController, private menuController: MenuController) {
+  // el alertcontroller es para las pantallaz de errores emergentes, bueno no necesariamente de errores
+  constructor(
+    private router: Router,
+    private alertcontroller: AlertController,
+    private menuController: MenuController) {
+      //No mostrar ninguno de los menu en esta pagina
     this.menuController.enable(false, 'MenuPrincipal')
     this.menuController.enable(false, 'MenuAdministrador')
-   }
-
-  async MostrarAlerta(message: string){
+  }
+//el metodo crea y muestra una alerta con un mensaje de error en el formulario
+  async MostrarAlerta(message: string) {
     const alerta = await this.alertcontroller.create({
       header: 'Error',
       message: message,
@@ -38,63 +36,52 @@ export class RegistrarsePage implements OnInit {
   ngOnInit() {
   }
 
-
-  validarCorreo(email: string){
+//de esta forma porque el any no es tan util para casos de seguridad
+  validarCorreo(email: string): boolean {
     const patron = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return patron.test(email)
+    return patron.test(email);
+  }
+//los boolean aca sirven para devolver el valor a si es verdadero o falso
+  validarContrasena(password: string): boolean {
+    const patron = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{6,}$/;
+    return patron.test(password);
   }
 
-  validarPassword(password: string){
-    const patron = /^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=..)[A-Za-z\d.]{6,}$/;
-    return patron.test(password)
+  validacionDatosCorreo() {
+    //Si hay algun campo vacio pide que se llenen todos o no saldran otras alertas
+    if (!this.username || !this.password || !this.email || !this.passwordR) {
+      this.MostrarAlerta('Todos los campos son obligatorios');
+      return;
+    }
+
+    if (!this.validarCorreo(this.email)) {
+      //pide que el correo este con el formato que tiene @
+      this.MostrarAlerta('Ingrese un correo válido');
+      return;
+    }
+
+    if (!this.validarContrasena(this.password)) {
+      this.MostrarAlerta('La contraseña debe tener al menos 6 caracteres, incluyendo una mayúscula, una minúscula, un número y un carácter especial.');
+      return;
+    }
+
+    if (this.password !== this.passwordR) {
+      this.MostrarAlerta('Las contraseñas no coinciden, intentelo nuevamente.');
+      return;
+    }
+
+    this.Registrarse();
   }
-
-  validacionDatosCorreo(){
-    if(!this.username || !this.password || !this.email || !this.passwordR){
-      this.MostrarAlerta('Se deben añadir todos los datos den los cuadros')
-      return;
-    }
-
-    if(!this.validarCorreo(this.email)){
-      this.ValidacionC = true
-      return;
-      }else{
-      this.ValidacionC = false
-      }
-
-
-
-    if(!this.validarPassword(this.password) || !this.validarPassword(this.passwordR)){
-      this.ValidarLargoP = true
-    } else{
-      this.ValidarLargoP = false
-      return;
-    }
-
-    if(this.password !== this.passwordR){
-      this.ValidarIgualdad = true
-    } else {
-      this.ValidarIgualdad = false
-      this.Registrarse()
-    }
-
-    }
-
-
-
-  Registrarse(){
+//Permite que los datos del user registrados esten disponibles en el login
+  Registrarse() {
     let navigationextras: NavigationExtras = {
-      state:{
-        username : this.username,
-        password :  this.password,
-        email : this.email
+      state: {
+        username: this.username,
+        password: this.password,
+        email: this.email
       }
-    }
-    
-    this.router.navigate(['/login'],navigationextras);
+    };
 
+    this.router.navigate(['/login'], navigationextras);
   }
-
-  
-
 }
